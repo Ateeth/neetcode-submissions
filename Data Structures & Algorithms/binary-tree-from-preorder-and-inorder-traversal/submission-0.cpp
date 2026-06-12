a3ebecce3e866ad5392e8fefee5561a032923b29
Ateeth Arun <58625369+Ateeth@users.noreply.github.com>
2026-06-12 13:07:45 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> mpp;
+        for(int i = 0 ; i < inorder.size(); ++i){
+            mpp[inorder[i]] = i;
+        }
+
+        TreeNode* root = buildTree(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, mpp);
+        return root;
+    }
+
+    TreeNode* buildTree(
+        vector<int>& preorder, int preStart, int preEnd,
+        vector<int>& inorder, int inStart, int inEnd, unordered_map<int, int>& mpp
+    ){  
+        if(preEnd < preStart || inEnd < inStart) return nullptr;
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int inorderIndex = mpp[root->val];
+        int numsLeft = inorderIndex - inStart;
+
+        root->left = buildTree(preorder, preStart+1, preStart + numsLeft, inorder, inStart,inorderIndex -1, mpp);
+        root->right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inorderIndex + 1, inEnd, mpp);
+
+        return root;
+    }
+};
