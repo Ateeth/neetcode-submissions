a72ba5b43de5710afb83791e1f78767d69393c73
Ateeth Arun <58625369+Ateeth@users.noreply.github.com>
2026-06-12 16:25:28 +0530

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> subset;
+        f(ans, subset, candidates, target, 0);
+        return ans;
+    }
+
+    void f(vector<vector<int>>& ans, vector<int>& subset, vector<int>& nums, int target, int idx){
+        if(target < 0) return;
+        if(target == 0) {
+            ans.push_back(subset);
+            return;
+        }
+        for(int i = idx ; i < nums.size(); ++i){
+            if(nums[i] > target) break;
+            if(i > idx && nums[i] == nums[i-1]) continue;
+            subset.push_back(nums[i]);
+            f(ans, subset, nums, target - nums[i], i + 1) ;
+            subset.pop_back();
+        }
+    }
+};
